14d6a2d62d3cf0f26c66f8d1eb608f0e33737b82
longpham <longpham3105@gmail.com>
2024-08-19 16:30:32 +0900

    Refactor

--- my_predict.py
+++ my_predict.py
@@ -32,6 +32,8 @@ def predict(args: argparse.Namespace):
     imgsz                          = args.imgsz[0]
     resize                         = args.resize
     benchmark                      = False  # args.benchmark
+    save_image                     = args.save_image
+    save_debug                     = args.save_debug
     use_fullpath                   = args.use_fullpath
     config_file                    = args.config_file
     # config_file                    = current_dir / "sam2_configs" / config_file
@@ -116,28 +118,29 @@ def predict(args: argparse.Namespace):
                 timer.tock()
                 
                 # Save
-                if use_fullpath:
-                    relative_path   = image_path.relative_path(data_name)
-                    binary_save_dir = save_dir / relative_path.parent / "binary"
-                    color_save_dir  = save_dir / relative_path.parent / "color"
-                else:
-                    binary_save_dir = save_dir / data_name / "binary"
-                    color_save_dir  = save_dir / data_name / "color"
-                # Binary
-                for i, mask in enumerate(masks):
-                    output_path = binary_save_dir / f"{image_path.stem}_mask_{i}.jpg"
+                if save_image:
+                    if use_fullpath:
+                        relative_path   = image_path.relative_path(data_name)
+                        binary_save_dir = save_dir / relative_path.parent / "binary"
+                        color_save_dir  = save_dir / relative_path.parent / "color"
+                    else:
+                        binary_save_dir = save_dir / data_name / "binary"
+                        color_save_dir  = save_dir / data_name / "color"
+                    # Binary
+                    for i, mask in enumerate(masks):
+                        output_path = binary_save_dir / f"{image_path.stem}_mask_{i}.jpg"
+                        output_path.parent.mkdir(parents=True, exist_ok=True)
+                        cv2.imwrite(str(output_path), np.uint8(mask["segmentation"]) * 255)
+                    # Color
+                    output          = np.ones((masks[0]["segmentation"].shape[0], masks[0]["segmentation"].shape[1], 4))
+                    output[:, :, 3] = 0
+                    for i, mask in enumerate(masks):
+                        mask_bool         = mask["segmentation"]
+                        color_mask        = np.concatenate([np.random.random(3), [1.0]])  # 0.35
+                        output[mask_bool] = color_mask
+                    output_path = color_save_dir / f"{image_path.stem}.jpg"
                     output_path.parent.mkdir(parents=True, exist_ok=True)
-                    cv2.imwrite(str(output_path), np.uint8(mask["segmentation"]) * 255)
-                # Color
-                output          = np.ones((masks[0]["segmentation"].shape[0], masks[0]["segmentation"].shape[1], 4))
-                output[:, :, 3] = 0
-                for i, mask in enumerate(masks):
-                    mask_bool         = mask["segmentation"]
-                    color_mask        = np.concatenate([np.random.random(3), [1.0]])  # 0.35
-                    output[mask_bool] = color_mask
-                output_path = color_save_dir / f"{image_path.stem}.jpg"
-                output_path.parent.mkdir(parents=True, exist_ok=True)
-                cv2.imwrite(str(output_path), np.uint8(output * 255))
+                    cv2.imwrite(str(output_path), np.uint8(output * 255))
         
         avg_time = float(timer.avg_time)
         console.log(f"Average time: {avg_time}")
